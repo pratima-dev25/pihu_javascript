41b5bf15d6d712ee69877785c204b02327f836d6
pratima-dev25 <krpratima655@gmail.com>
2026-02-03 06:42:57 +0000

    variable and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState; 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS codde as nnewer version
+
+// alert("hello")// we are using nodeJs, not browser
+
+// TC39 & mdn ;- for documentation
+
+// Data Types...
+
+let name = "hitesh"
+let age = 18
+let isLoggedId = true
+let state;
+let state1 =null
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+// boolean => true/false
+//null => standlone values
+//undefined => 
+//symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefine
+console.log(typeof null); // object
